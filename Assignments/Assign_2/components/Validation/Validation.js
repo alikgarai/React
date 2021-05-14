0d2dd1c587aacc81bb99ff2a93f6cada859ded9c
alikgarai <garaialik247@gmail.com>
2021-05-14 13:42:29 +0530

    Assignment 2 completed

--- Assignments/Assign_2/components/Validation/Validation.js
+++
@@ -1,8 +0,0 @@
-import "../../containers/App.css";
-
-const component = (props) => {
-  let str = props.count > 5 ? "Text long enough" : "Text too short";
-  return <p className="Main">{str}</p>;
-};
-
-export default component;
